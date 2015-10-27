ce397000b7a8b50efa950637d76335b299bd0d7b
Oskar Hane <oh@oskarhane.com>
2015-10-27 07:58:31 +0100

    Fix utf8-decode on CombinedBuffers for nodejs.

--- test/internal/utf8.test.js
+++ test/internal/utf8.test.js
@@ -1,5 +1,6 @@
 
 var utf8 = require('../../build/node/internal/utf8');
+var buffers = require('../../build/node/internal/buf');
 
 describe('utf8', function() {
   it('should have a nice clean buffer position after serializing', function() {
@@ -13,6 +14,40 @@ describe('utf8', function() {
   it('should read/write utf8', function() {
     expect( packAndUnpack( "" ) ).toBe( "" );
     expect( packAndUnpack( "åäö123" ) ).toBe( "åäö123"  );
+  });
+
+  it('should read/write utf8 from a complete combined buffer', function() {
+    // Given
+    var msg = "asåfqwer";
+    var buf = utf8.encode(msg);
+    var bufa = buf.readSlice(3);
+    var bufb = buf.readSlice(3);
+    var bufc = buf.readSlice(3);
+    var combined = new buffers.CombinedBuffer( [bufa, bufb, bufc] );
+
+    // When
+    var decoded = utf8.decode(combined, combined.length);
+
+    // Then
+    expect(decoded).toBe(msg);
+  });
+
+  it('should read/write utf8 from part of a combined buffer', function() {
+    // Given
+    var msg = "asåfq";
+    var expectMsg = msg.substring(0, msg.length-1);
+    var buf = utf8.encode(msg);
+    var bufa = buf.readSlice(3);
+    var bufb = buf.readSlice(3);
+    var unrelatedData = buffers.alloc(3);
+    var combined = new buffers.CombinedBuffer( [bufa, bufb, unrelatedData] );
+
+    // When 
+    // We read all but the unrelatedData and the last character of bufb
+    var decoded = utf8.decode(combined, combined.length - 1 - unrelatedData.length );
+
+    // Then
+    expect(decoded).toBe(expectMsg);
   });
 });
 
