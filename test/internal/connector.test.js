a454f26572b664ad71b7b8dfa4097d0da6fcfc74
lutovich <konstantin.lutovich@neotechnology.com>
2017-02-15 14:51:54 +0100

    Move connector tests to ES6

--- test/internal/connector.test.js
+++ test/internal/connector.test.js
@@ -16,43 +16,43 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var DummyChannel = require('../../lib/v1/internal/ch-dummy.js');
-var connect = require("../../lib/v1/internal/connector.js").connect;
 
-describe('connector', function() {
+import * as DummyChannel from "../../src/v1/internal/ch-dummy";
+import {connect} from "../../src/v1/internal/connector";
 
-  it('should read/write basic messages', function(done) {
+describe('connector', () => {
+
+  it('should read/write basic messages', done => {
     // Given
-    var conn = connect("bolt://localhost")
+    const conn = connect("bolt://localhost");
 
     // When
-    conn.initialize( "mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"},  {
-      onCompleted: function( msg ) {
-        expect( msg ).not.toBeNull();
+    conn.initialize("mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"}, {
+      onCompleted: msg => {
+        expect(msg).not.toBeNull();
         conn.close();
         done();
       },
-      onError: function(err) {
-        console.log(err);
-      }
+      onError: console.log
     });
     conn.sync();
 
   });
-  it('should retrieve stream', function(done) {
+
+  it('should retrieve stream', done => {
     // Given
-    var conn = connect("bolt://localhost")
+    const conn = connect("bolt://localhost");
 
     // When
-    var records = [];
-    conn.initialize( "mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"} );
-    conn.run( "RETURN 1.0", {} );
-    conn.pullAll( {
-      onNext: function( record ) {
-        records.push( record );
+    const records = [];
+    conn.initialize("mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"});
+    conn.run("RETURN 1.0", {});
+    conn.pullAll({
+      onNext: record => {
+        records.push(record);
       },
-      onCompleted: function( tail ) {
-        expect( records[0][0] ).toBe( 1 );
+      onCompleted: () => {
+        expect(records[0][0]).toBe(1);
         conn.close();
         done();
       }
@@ -60,31 +60,30 @@ describe('connector', function() {
     conn.sync();
   });
 
-  it('should use DummyChannel to read what gets written', function(done) {
+  it('should use DummyChannel to read what gets written', done => {
     // Given
-    var observer = DummyChannel.observer;
-    var conn = connect("bolt://localhost", {channel:DummyChannel.channel});
+    const observer = DummyChannel.observer;
+    const conn = connect("bolt://localhost", {channel: DummyChannel.channel});
 
     // When
-    var records = [];
-    conn.initialize( "mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"} );
-    conn.run( "RETURN 1", {} );
+    conn.initialize("mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"});
+    conn.run("RETURN 1", {});
     conn.sync();
-    expect( observer.instance.toHex() ).toBe( '60 60 b0 17 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 41 b2 01 8e 6d 79 64 72 69 76 65 72 2f 30 2e 30 2e 30 a3 86 73 63 68 65 6d 65 85 62 61 73 69 63 89 70 72 69 6e 63 69 70 61 6c 85 6e 65 6f 34 6a 8b 63 72 65 64 65 6e 74 69 61 6c 73 85 6e 65 6f 34 6a 00 00 00 0c b2 10 88 52 45 54 55 52 4e 20 31 a0 00 00 ' );
+    expect(observer.instance.toHex()).toBe('60 60 b0 17 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 41 b2 01 8e 6d 79 64 72 69 76 65 72 2f 30 2e 30 2e 30 a3 86 73 63 68 65 6d 65 85 62 61 73 69 63 89 70 72 69 6e 63 69 70 61 6c 85 6e 65 6f 34 6a 8b 63 72 65 64 65 6e 74 69 61 6c 73 85 6e 65 6f 34 6a 00 00 00 0c b2 10 88 52 45 54 55 52 4e 20 31 a0 00 00 ');
     done();
   });
 
-  it('should provide error message when connecting to http-port', function(done) {
+  it('should provide error message when connecting to http-port', done => {
     // Given
-    var conn = connect("bolt://localhost:7474", {encrypted:false});
+    const conn = connect("bolt://localhost:7474", {encrypted: false});
 
     // When
-    conn.initialize( "mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"},  {
-      onCompleted: function( msg ) {
+    conn.initialize("mydriver/0.0.0", {scheme: "basic", principal: "neo4j", credentials: "neo4j"}, {
+      onCompleted: msg => {
       },
-      onError: function(err) {
+      onError: err => {
         //only node gets the pretty error message
-        if( require('../../lib/v1/internal/ch-node.js').available ) {
+        if (require('../../lib/v1/internal/ch-node.js').available) {
           expect(err.message).toBe("Server responded HTTP. Make sure you are not trying to connect to the http endpoint " +
             "(HTTP defaults to port 7474 whereas BOLT defaults to port 7687)");
         }
