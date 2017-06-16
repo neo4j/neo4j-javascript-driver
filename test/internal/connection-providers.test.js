79585223b4fa1e30065c204788cce3eee6f6fe43
lutovich <konstantin.lutovich@neotechnology.com>
2017-06-16 19:01:31 +0200

    Couple unit tests for connection providers
    To assert that both `DirectConnectionProvider` and `LoadBalancer`
    return promises with initialized connections.

--- test/internal/connection-providers.test.js
+++ test/internal/connection-providers.test.js
@@ -44,6 +44,16 @@ describe('DirectConnectionProvider', () => {
     });
   });
 
+  it('returns an initialized connection', done => {
+    const pool = newPool();
+    const connectionProvider = newDirectConnectionProvider('localhost:123', pool);
+
+    connectionProvider.acquireConnection(READ).then(connection => {
+      expect(connection.initialized).toBeTruthy();
+      done();
+    });
+  });
+
 });
 
 describe('LoadBalancer', () => {
@@ -1049,6 +1059,26 @@ describe('LoadBalancer', () => {
     });
   });
 
+  it('returns an initialized connection', done => {
+    const pool = newPool();
+    const loadBalancer = newLoadBalancer(
+      ['server-1', 'server-2'],
+      ['server-3', 'server-4'],
+      ['server-5', 'server-6'],
+      pool
+    );
+
+    loadBalancer.acquireConnection(READ).then(connection => {
+      expect(connection.initialized).toBeTruthy();
+
+      loadBalancer.acquireConnection(WRITE).then(connection => {
+        expect(connection.initialized).toBeTruthy();
+
+        done();
+      });
+    });
+  });
+
 });
 
 function newDirectConnectionProvider(address, pool) {
@@ -1126,6 +1156,7 @@ class FakeConnection {
   constructor(address, release) {
     this.address = address;
     this.release = release;
+    this.initialized = false;
   }
 
   static create(address, release) {
@@ -1133,6 +1164,7 @@ class FakeConnection {
   }
 
   initializationCompleted() {
+    this.initialized = true;
     return Promise.resolve(this);
   }
 }
