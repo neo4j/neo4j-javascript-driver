8c15494a3450b401ebe329f1e0bc779ad75b1ad1
lutovich <konstantin.lutovich@neotechnology.com>
2018-01-15 11:58:41 +0100

    Improve transaction terminated error assertion
    Message has changed in neo4j 3.4, which made couple session tests fail.

--- test/v1/session.test.js
+++ test/v1/session.test.js
@@ -1083,7 +1083,9 @@ describe('session', () => {
   }
 
   function expectTransactionTerminatedError(error) {
-    expect(error.message.toLowerCase().indexOf('transaction terminated') >= 0).toBeTruthy();
+    const message = error.message.toLowerCase();
+    expect(message.indexOf('transaction')).not.toBeLessThan(0);
+    expect(message.indexOf('terminated')).not.toBeLessThan(0);
   }
 
   function readAllNodeIds() {
