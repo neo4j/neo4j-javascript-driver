5ffcad7a2223938be3e9e3c70b54c3b0e5e40c94
Ali Ince <ali.ince@neotechnology.com>
2019-04-04 14:13:52 +0100

    Prevent testing for bolt+routing failure against 4.0 server

--- test/driver.test.js
+++ test/driver.test.js
@@ -22,7 +22,7 @@ import sharedNeo4j from './internal/shared-neo4j';
 import FakeConnection from './internal/fake-connection';
 import lolex from 'lolex';
 import {DEFAULT_ACQUISITION_TIMEOUT, DEFAULT_MAX_SIZE} from '../src/internal/pool-config';
-import {ServerVersion, VERSION_3_1_0} from '../src/internal/server-version';
+import {ServerVersion, VERSION_3_1_0, VERSION_4_0_0} from '../src/internal/server-version';
 import testUtils from './internal/test-utils';
 
 describe('driver', () => {
@@ -187,6 +187,11 @@ describe('driver', () => {
   });
 
   it('should fail nicely when connecting with routing to standalone server', done => {
+    if (!routingProcedureOnlyAvailableOnCores()) {
+      done();
+      return;
+    }
+
     // Given
     driver = neo4j.driver("bolt+routing://localhost", sharedNeo4j.authToken);
 
@@ -462,6 +467,10 @@ describe('driver', () => {
 
   function openConnectionFrom(driver) {
     return Array.from(Object.values(driver._openConnections));
+  }
+
+  function routingProcedureOnlyAvailableOnCores() {
+    return serverVersion.compareTo(VERSION_4_0_0) < 0;
   }
 
 });
