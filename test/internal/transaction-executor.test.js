643ade5207861118c6b0be8a5e96e12be26939f4
Ali Ince <ali.ince@neotechnology.com>
2019-09-30 18:18:16 +0100

    Add a test for retries on unknown exceptions

--- test/internal/transaction-executor.test.js
+++ test/internal/transaction-executor.test.js
@@ -91,6 +91,23 @@ describe('TransactionExecutor', () => {
     testNoRetryOnUnknownError([LOCKS_TERMINATED_ERROR], 1, done)
   })
 
+  it('should not retry when transaction work returns promise rejected with unknown error type', async () => {
+    class MyTestError extends Error {
+      constructor (message, code) {
+        super(message)
+        this.code = code
+      }
+    }
+
+    const error = new MyTestError('an unexpected error', 504)
+    const executor = new TransactionExecutor()
+    const realWork = () => Promise.reject(error)
+
+    await expectAsync(
+      executor.execute(transactionCreator(), tx => realWork())
+    ).toBeRejectedWith(error)
+  })
+
   it('should stop retrying when time expires', done => {
     const executor = new TransactionExecutor()
     const usedTransactions = []
