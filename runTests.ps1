6351f196e2405f1832c5f58494156dae7e5d6ee2
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-06-12 13:52:23 +0200

    Run only unit tests on Windows

--- runTests.ps1
+++ runTests.ps1
@@ -2,33 +2,34 @@ npm ci
 
 $ErrorFound = $False
 
-try
-{
-	If ($args.Length -gt 0)
-	{
-		$env:NEOCTRL_ARGS="$args"
-	}
-
-	npm run start-neo4j
-	if($LastExitCode -ne 0) #failed to execute npm test without error
-	{
-		Write-Host "Unable to start neo4j"
-		exit 1
-	}
-
-	npm test
-	if($LastExitCode -ne 0) #failed to execute npm test without error
-	{
-		$ErrorFound = $True
-	}
-}
-finally
-{
-	npm run stop-neo4j
-	if($ErrorFound)
-	{
-		Write-Host "Exit with code 1"
-		exit 1
-	}
-	Write-Host "Exit with code 0"
-}
+#try
+#{
+#	If ($args.Length -gt 0)
+#	{
+#		$env:NEOCTRL_ARGS="$args"
+#	}
+#
+#	npm run start-neo4j
+#	if($LastExitCode -ne 0) #failed to execute npm test without error
+#	{
+#		Write-Host "Unable to start neo4j"
+#		exit 1
+#	}
+#
+#	npm test
+#	if($LastExitCode -ne 0) #failed to execute npm test without error
+#	{
+#		$ErrorFound = $True
+#	}
+#}
+#finally
+#{
+#	npm run stop-neo4j
+#	if($ErrorFound)
+#	{
+#		Write-Host "Exit with code 1"
+#		exit 1
+#	}
+#	Write-Host "Exit with code 0"
+#}
+gulp test-nodejs-unit
