88e646d4664943e337aecb7d61c187407926cc46
Mark Needham <m.h.needham@gmail.com>
2020-07-02 14:59:24 +0100

    Aura example (#594)

--- test/examples.test.js
+++ test/examples.test.js
@@ -405,6 +405,73 @@ describe('#integration examples', () => {
     expect(await consoleLoggedMsg).toEqual(personName)
   })
 
+  it('driver introduction example', async () => {
+    const console = consoleOverride
+    const consoleLoggedMsg = consoleOverridePromise
+
+    // tag::driver-introduction-example-imports[]
+    const neo4j = require('neo4j-driver')
+    // end::driver-introduction-example-imports[]
+
+    // tag::driver-introduction-example-variables[]
+    /*
+    const uri = '%%BOLT_URL_PLACEHOLDER%%';
+    const user = '<Username for Neo4j Aura database>';
+    const password = '<Password for Neo4j Aura database>';
+    */
+    // end::driver-introduction-example-variables[]
+
+    // tag::driver-introduction-example[]
+    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+    const session = driver.session()
+
+    const person1Name = 'Alice'
+    const person2Name = 'David'
+
+    try {
+      // To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
+      // The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
+      const writeQuery = `MERGE (p1:Person { name: $person1Name })
+                          MERGE (p2:Person { name: $person2Name })
+                          MERGE (p1)-[:KNOWS]->(p2)
+                          RETURN p1, p2`
+
+      // Write transactions allow the driver to handle retries and transient errors
+      const writeResult = await session.writeTransaction(tx =>
+        tx.run(writeQuery, { person1Name, person2Name })
+      )
+      writeResult.records.forEach(record => {
+        const person1Node = record.get('p1')
+        const person2Node = record.get('p2')
+        console.log(
+          `Created friendship between: ${person1Node.properties.name}, ${person2Node.properties.name}`
+        )
+      })
+
+      const readQuery = `MATCH (p:Person)
+                         WHERE p.name = $personName
+                         RETURN p.name AS name`
+      const readResult = await session.readTransaction(tx =>
+        tx.run(readQuery, { personName: person1Name })
+      )
+      readResult.records.forEach(record => {
+        console.log(`Found person: ${record.get('name')}`)
+      })
+    } catch (error) {
+      console.error('Something went wrong: ', error)
+    } finally {
+      await session.close()
+    }
+
+    // Don't forget to close the driver connection when you're finished with it
+    await driver.close()
+    // end::driver-introduction-example[]
+
+    expect(await consoleLoggedMsg).toEqual(
+      `Created friendship between: ${person1Name}, ${person2Name}`
+    )
+  })
+
   it('read write transaction example', async () => {
     const console = consoleOverride
     const consoleLoggedMsg = consoleOverridePromise
