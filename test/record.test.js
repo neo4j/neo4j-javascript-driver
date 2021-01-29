694b9ea4ba69c74c9079cb37a1e1fd23f79a0625
Antonio Barcélos <antonio.barcelos@neo4j.com>
2021-01-29 09:57:58 +0100

    Remove year from the license header (#673)

--- test/record.test.js
+++ test/record.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2002-2020 "Neo4j,"
+ * Copyright (c) "Neo4j"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
@@ -111,7 +111,10 @@ describe('#unit Record', () => {
     })
 
     // Then
-    expect(result).toEqual([['Bob', 'name', record], [45, 'age', record]])
+    expect(result).toEqual([
+      ['Bob', 'name', record],
+      [45, 'age', record]
+    ])
   })
 
   it('should allow map function for the record', () => {
@@ -124,7 +127,10 @@ describe('#unit Record', () => {
     })
 
     // Then
-    expect(result).toEqual([['Bob', 'name', record], [45, 'age', record]])
+    expect(result).toEqual([
+      ['Bob', 'name', record],
+      [45, 'age', record]
+    ])
   })
 
   it('should allow taking values lazily', () => {
