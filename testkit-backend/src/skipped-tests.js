48a1dcd6e7bf3d98445164babe1ef8c1f2671f2c
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-04-01 16:23:14 +0200

    testkit-backend: Move all the skipTest to backend
    The goal of this changes is make all driver specific code part of the driver repository.

--- testkit-backend/src/skipped-tests.js
+++ testkit-backend/src/skipped-tests.js
@@ -2,11 +2,27 @@ function ifEndsWith (suffix) {
   return testName => testName.endsWith(suffix)
 }
 
+function ifStartsWith (prefix) {
+  return testName => testName.startsWith(prefix)
+}
+
+function ifEquals (expectedName) {
+  return testName => testName === expectedName
+}
+
+function or () {
+  return testName => [...arguments].find(predicate => predicate(testName))
+}
+
 function skip (reason, predicate) {
   return { reason, predicate }
 }
 
 const skippedTests = [
+  skip(
+    'Routing tests are disabled until the fix on the test scenario be merged',
+    or(ifStartsWith('stub.routing'), ifStartsWith('stub.retry.TestRetry'))
+  ),
   skip(
     'Driver is failing trying to update the routing table using the original routing server',
     ifEndsWith(
@@ -18,6 +34,65 @@ const skippedTests = [
     ifEndsWith(
       'test_should_successfully_check_if_support_for_multi_db_is_available'
     )
+  ),
+  skip(
+    'Driver should implement resolver',
+    or(
+      ifEndsWith(
+        'test_should_revert_to_initial_router_if_known_router_throws_protocol_errors'
+      ),
+      ifEndsWith(
+        'test_should_use_resolver_during_rediscovery_when_existing_routers_fail'
+      )
+    )
+  ),
+  skip(
+    'Test are not consuming the values inside the try catch',
+    or(
+      ifEndsWith('test_should_retry_read_tx_and_rediscovery_until_success'),
+      ifEndsWith('test_should_retry_write_tx_and_rediscovery_until_success'),
+      ifEndsWith('test_should_retry_write_tx_until_success'),
+      ifEndsWith(
+        'test_should_read_successfully_from_reachable_db_after_trying_unreachable_db'
+      ),
+      ifEndsWith(
+        'test_should_retry_write_until_success_with_leader_shutdown_during_tx_using_tx_function'
+      )
+    )
+  ),
+  skip(
+    'Requires investigation',
+    or(
+      ifEndsWith(
+        'test_should_fail_when_writing_without_explicit_consumption_on_writer_that_returns_not_a_leader_code'
+      ),
+      ifEndsWith(
+        'test_should_fail_when_writing_on_unexpectedly_interrupting_writer_using_session_run'
+      ),
+      ifEndsWith(
+        'test_should_fail_with_routing_failure_on_db_not_found_discovery_failure'
+      ),
+      ifEndsWith(
+        'test_should_retry_write_until_success_with_leader_change_using_tx_function'
+      ),
+      ifEndsWith(
+        'test_should_request_rt_from_all_initial_routers_until_successful'
+      ),
+      ifEndsWith('test_should_pass_bookmark_from_tx_to_tx_using_tx_run'),
+      ifEndsWith('test_should_successfully_get_routing_table_with_context')
+    )
+  ),
+  skip(
+    'Should implement result.consume',
+    ifEquals('neo4j.sessionrun.TestSessionRun.test_updates_last_bookmark')
+  ),
+  skip(
+    'It could not guarantee the order of records requests between in the nested transactions',
+    ifEquals('stub.iteration.TxRun.test_nested')
+  ),
+  skip(
+    'Keeps retrying on commit despite connection being dropped',
+    ifEquals('stub.retry.TestRetry.test_disconnect_on_commit')
   )
 ]
 
