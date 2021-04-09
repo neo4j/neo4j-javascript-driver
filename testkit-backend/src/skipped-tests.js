97fa3636b42b905fb8036d7a8e71b9863dd2606f
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-04-09 14:21:33 +0200

    Update reason why test_should_use_initial_router_for_discovery_when_others_unavailable is skipped

--- testkit-backend/src/skipped-tests.js
+++ testkit-backend/src/skipped-tests.js
@@ -30,12 +30,6 @@ const skippedTests = [
     )
   ),
   skip(
-    'Driver is failing trying to update the routing table using the original routing server',
-    ifEndsWith(
-      'test_should_use_initial_router_for_discovery_when_others_unavailable'
-    )
-  ),
-  skip(
     'Driver is creating a dedicated connection to check the MultiDBSupport',
     ifEndsWith(
       'test_should_successfully_check_if_support_for_multi_db_is_available'
@@ -81,6 +75,12 @@ const skippedTests = [
   skip(
     'Keeps retrying on commit despite connection being dropped',
     ifEquals('stub.retry.TestRetry.test_disconnect_on_commit')
+  ),
+  skip(
+    'Wait clarification about verifyConnective behaviour when no reader connection is available',
+    ifEndsWith(
+      '.test_should_use_initial_router_for_discovery_when_others_unavailable'
+    )
   )
 ]
 
