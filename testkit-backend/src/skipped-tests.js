9b16a7664ceab56ba8081efa7b8ce5cf5b9ada9a
Antonio Barcélos <antonio.barcelos@neo4j.com>
2021-04-27 15:59:39 +0200

    Re-enabling skipped tests (#704)
    Some tests were disabled for different reasons, the objective of this PR is to re-enable some skipped tests. 
    
    This PR re-enable tests related to:
    
    - ROUTE message receiving the bookmarks
    - HELLO message receiving the initial address in the routing context
    - Routing tests which were skipped because of the lack of result consumption and wrong try_count verification

--- testkit-backend/src/skipped-tests.js
+++ testkit-backend/src/skipped-tests.js
@@ -14,23 +14,25 @@ function or () {
   return testName => [...arguments].find(predicate => predicate(testName))
 }
 
-function skip (reason, predicate) {
-  return { reason, predicate }
+function skip (reason, ...predicate) {
+  return { reason, predicate: or(...predicate) }
 }
 
 const skippedTests = [
   skip(
-    'Routing tests are disabled until the fix on the test scenario be merged',
-    or(ifStartsWith('stub.routing'), ifStartsWith('stub.retry.TestRetry'))
-  ),
-  skip(
-    'The driver no support domain_name_resolver',
-    ifEndsWith('test_should_successfully_acquire_rt_when_router_ip_changes')
-  ),
-  skip(
-    'Driver is failing trying to update the routing table using the original routing server',
+    'The driver has no support domain_name_resolver',
+    ifEndsWith('test_should_successfully_acquire_rt_when_router_ip_changes'),
     ifEndsWith(
-      'test_should_use_initial_router_for_discovery_when_others_unavailable'
+      'test_should_request_rt_from_all_initial_routers_until_successful'
+    )
+  ),
+  skip(
+    'Driver is start to run tx_function before have a valid connection in hands',
+    ifEndsWith(
+      'test_should_retry_write_until_success_with_leader_shutdown_during_tx_using_tx_function'
+    ),
+    ifEndsWith(
+      'test_should_retry_write_until_success_with_leader_change_using_tx_function'
     )
   ),
   skip(
@@ -40,56 +42,10 @@ const skippedTests = [
     )
   ),
   skip(
-    'Driver should implement resolver',
-    or(
-      ifEndsWith(
-        'test_should_revert_to_initial_router_if_known_router_throws_protocol_errors'
-      ),
-      ifEndsWith(
-        'test_should_use_resolver_during_rediscovery_when_existing_routers_fail'
-      )
+    'Fails when runned with the full suite',
+    ifEndsWith(
+      '.test_should_revert_to_initial_router_if_known_router_throws_protocol_errors'
     )
-  ),
-  skip(
-    'Test are not consuming the values inside the try catch',
-    or(
-      ifEndsWith('test_should_retry_read_tx_and_rediscovery_until_success'),
-      ifEndsWith('test_should_retry_write_tx_and_rediscovery_until_success'),
-      ifEndsWith('test_should_retry_write_tx_until_success'),
-      ifEndsWith(
-        'test_should_read_successfully_from_reachable_db_after_trying_unreachable_db'
-      ),
-      ifEndsWith(
-        'test_should_retry_write_until_success_with_leader_shutdown_during_tx_using_tx_function'
-      )
-    )
-  ),
-  skip(
-    'Requires investigation',
-    or(
-      ifEndsWith(
-        'test_should_fail_when_writing_without_explicit_consumption_on_writer_that_returns_not_a_leader_code'
-      ),
-      ifEndsWith(
-        'test_should_fail_when_writing_on_unexpectedly_interrupting_writer_using_session_run'
-      ),
-      ifEndsWith(
-        'test_should_fail_with_routing_failure_on_db_not_found_discovery_failure'
-      ),
-      ifEndsWith(
-        'test_should_retry_write_until_success_with_leader_change_using_tx_function'
-      ),
-      ifEndsWith(
-        'test_should_request_rt_from_all_initial_routers_until_successful'
-      ),
-      ifEndsWith('test_should_pass_bookmark_from_tx_to_tx_using_tx_run'),
-      ifEndsWith('test_should_successfully_get_routing_table_with_context'),
-      ifStartsWith('stub.transport.Transport')
-    )
-  ),
-  skip(
-    'Should implement result.consume',
-    ifEquals('neo4j.sessionrun.TestSessionRun.test_updates_last_bookmark')
   ),
   skip(
     'It could not guarantee the order of records requests between in the nested transactions',
@@ -98,6 +54,29 @@ const skippedTests = [
   skip(
     'Keeps retrying on commit despite connection being dropped',
     ifEquals('stub.retry.TestRetry.test_disconnect_on_commit')
+  ),
+  skip(
+    'Wait clarification about verifyConnectivity behaviour when no reader connection is available',
+    ifEndsWith(
+      '.test_should_use_initial_router_for_discovery_when_others_unavailable'
+    ),
+    ifEndsWith('.test_should_successfully_get_routing_table_with_context')
+  ),
+  skip(
+    'Driver is executing the second tx for the same session in a diferent server',
+    ifEndsWith('test_should_pass_bookmark_from_tx_to_tx_using_tx_run')
+  ),
+  skip(
+    'Driver resolves the address during the record fetch',
+    ifEndsWith(
+      'test_should_use_resolver_during_rediscovery_when_existing_routers_fail'
+    )
+  ),
+  skip(
+    'Needs investigation. It is only failing in the RoutingV3 case',
+    ifEndsWith(
+      'RoutingV3.test_should_accept_routing_table_without_writers_and_then_rediscover'
+    )
   )
 ]
 
