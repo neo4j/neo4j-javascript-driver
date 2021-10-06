9df415214b7e8091508f3101619bd749dfe16c4b
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-10-06 12:02:12 +0200

    Fix driver selection in the testkit-backend

--- packages/testkit-backend/src/neo4j.js
+++ packages/testkit-backend/src/neo4j.js
@@ -1,7 +1,7 @@
 import neo4jDriver from 'neo4j-driver'
 import neo4jDriverLite from 'neo4j-driver-lite'
 
-const isLite = (process.env.TEST_DRIVER_LITE || 'False').toUpperCase() in ['TRUE', '1']
+const isLite = ['TRUE', '1'].includes((process.env.TEST_DRIVER_LITE || 'False').toUpperCase())
 const neo4j = isLite ? neo4jDriverLite : neo4jDriver
 
 export default neo4j
