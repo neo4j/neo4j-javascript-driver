0f02fefc8e240037510c0706734852977e5fd853
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-10-15 11:07:27 +0200

    Running prepare only  if user is root

--- runTests.sh
+++ runTests.sh
@@ -7,7 +7,14 @@ trap finish EXIT
 
 npm ci
 npm run build -- --no-private
-npm run lerna -- run prepare --no-private
+
+# root users could not run the lifecycle scripts
+# so it need will need to run the prepare script 
+if [ "$EUID" -e 0 ]
+  then echo "Running prepare by manually"
+  npm run lerna -- run prepare --no-private  
+fi
+
 
 if [[ ! -z "$1" ]]; then
   export NEOCTRL_ARGS="$1"
