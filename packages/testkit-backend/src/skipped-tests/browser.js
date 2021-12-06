f277d3284f47a7e70ef4e960ea07e80c12a3b07c
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-12-06 17:03:52 +0100

    Disable tests related to socket timeouts

--- packages/testkit-backend/src/skipped-tests/browser.js
+++ packages/testkit-backend/src/skipped-tests/browser.js
@@ -1,6 +1,10 @@
 import skip, { ifStartsWith } from './skip'
 const skippedTests = [
   skip(
+    'Browser doesn\'t support socket timeouts',
+    ifStartsWith('stub.configuration_hints.test_connection_recv_timeout_seconds.TestDirectConnectionRecvTimeout')
+  ),
+  skip(
     'TLS Tests not implemented for browwer',
     ifStartsWith('tls')
   )
