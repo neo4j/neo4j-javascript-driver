2462329f0bc66193706acfabe7689aba9946486e
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-12-06 19:49:47 +0100

    Skip ttests

--- packages/testkit-backend/src/skipped-tests/browser.js
+++ packages/testkit-backend/src/skipped-tests/browser.js
@@ -1,12 +1,13 @@
-import skip, { ifEndsWith, ifStartsWith } from './skip'
+import skip, { ifEndsWith, ifEquals, ifStartsWith } from './skip'
 const skippedTests = [
   skip(
     'Browser doesn\'t support socket timeouts',
-    ifStartsWith('stub.configuration_hints.test_connection_recv_timeout_seconds.TestDirectConnectionRecvTimeout')
+    ifStartsWith('stub.configuration_hints.test_connection_recv_timeout_seconds')
   ),
   skip(
     'Investigate why websocket is getting too much time to close the connection',
-    ifEndsWith('test_should_check_multi_db_support')
+    ifEndsWith('test_should_check_multi_db_support'),
+    ifEquals('stub.disconnects.test_disconnects.TestDisconnects.test_fail_on_reset')
   ),
   skip(
     'TLS Tests not implemented for browwer',
