a0f7ac1bbbd85e930ebb2e56c6a2df3ca357b09b
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-12-06 17:40:28 +0100

    Skipping tests related to websocket take too much time to close the connection

--- packages/testkit-backend/src/skipped-tests/browser.js
+++ packages/testkit-backend/src/skipped-tests/browser.js
@@ -1,8 +1,12 @@
-import skip, { ifStartsWith } from './skip'
+import skip, { ifEndsWith, ifStartsWith } from './skip'
 const skippedTests = [
   skip(
     'Browser doesn\'t support socket timeouts',
     ifStartsWith('stub.configuration_hints.test_connection_recv_timeout_seconds.TestDirectConnectionRecvTimeout')
+  ),
+  skip(
+    'Investigate why websocket is getting too much time to close the connection',
+    ifEndsWith('test_should_check_multi_db_support')
   ),
   skip(
     'TLS Tests not implemented for browwer',
