54c9a4ec1926da8524e1372eca1493bd68604e04
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-12-06 16:06:02 +0100

    Enabling stub tests for browser

--- packages/testkit-backend/src/skipped-tests/browser.js
+++ packages/testkit-backend/src/skipped-tests/browser.js
@@ -1,10 +1,6 @@
 import skip, { ifStartsWith } from './skip'
 const skippedTests = [
   skip(
-    'Stub Tests not implemented for browser',
-    ifStartsWith('stub')
-  ),
-  skip(
     'TLS Tests not implemented for browwer',
     ifStartsWith('tls')
   )
