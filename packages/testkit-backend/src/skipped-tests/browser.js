beab63cc98cdba1d1facc986844e46007419b733
Antonio Barcélos <antonio.barcelos@neo4j.com>
2021-12-10 11:23:07 -0300

    Enabling stub tests for browser (#821)
    Co-authored-by: Robsdedude <dev@rouvenbauer.de>

--- packages/testkit-backend/src/skipped-tests/browser.js
+++ packages/testkit-backend/src/skipped-tests/browser.js
@@ -1,8 +1,16 @@
-import skip, { ifStartsWith } from './skip'
+import skip, { ifEndsWith, ifEquals, ifStartsWith } from './skip'
 const skippedTests = [
   skip(
-    'Stub Tests not implemented for browser',
-    ifStartsWith('stub')
+    "Browser doesn't support socket timeouts",
+    ifStartsWith('stub.configuration_hints.test_connection_recv_timeout_seconds')
+  ),
+  skip(
+    'Investigate why websocket is taking too much time to close the connection',
+    ifEndsWith('test_should_check_multi_db_support'),
+    ifEquals('stub.disconnects.test_disconnects.TestDisconnects.test_fail_on_reset'),
+    ifEquals('stub.tx_begin_parameters.test_tx_begin_parameters.TestTxBeginParameters.test_impersonation_fails_on_v4x3'),
+    ifEquals('stub.session_run_parameters.test_session_run_parameters.TestSessionRunParameters.test_impersonation_fails_on_v4x3'),
+    
   ),
   skip(
     'TLS Tests not implemented for browwer',
