723fe14f5cb718131e6a89ecaebb4879d00110ad
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-02-17 18:16:52 +0100

    Add rx handlers for: NewSession, SessionClose, SessionRun

--- packages/testkit-backend/src/request-handlers-rx.js
+++ packages/testkit-backend/src/request-handlers-rx.js
@@ -1,3 +1,6 @@
+import * as responses from './responses.js';
+import neo4j from './neo4j.js';
+
 // Handlers which didn't change depending
 export {
   NewDriver,
@@ -11,5 +14,62 @@ export {
   ResolverResolutionCompleted,
   GetRoutingTable,
   ForcedRoutingTableUpdate,
-} from './request-handlers';
+} from './request-handlers.js';
 
+export function NewSession(context, data, wire) {
+  let { driverId, accessMode, bookmarks, database, fetchSize, impersonatedUser } = data
+  switch (accessMode) {
+    case 'r':
+      accessMode = neo4j.session.READ
+      break
+    case 'w':
+      accessMode = neo4j.session.WRITE
+      break
+    default:
+      wire.writeBackendError('Unknown accessmode: ' + accessMode)
+      return
+  }
+  const driver = context.getDriver(driverId)
+  const session = driver.rxSession({
+    defaultAccessMode: accessMode,
+    bookmarks,
+    database,
+    fetchSize,
+    impersonatedUser
+  })
+  const id = context.addSession(session)
+  wire.writeResponse(responses.Session({ id }))
+}
+
+export function SessionClose (context, data, wire) {
+  const { sessionId } = data
+  const session = context.getSession(sessionId)
+  return session
+    .close()
+    .toPromise()
+    .then(() => wire.writeResponse(responses.Session({ id: sessionId })))
+    .catch(err => wire.writeError(err))
+}
+
+export function SessionRun (context, data, wire) {
+  const { sessionId, cypher, params, txMeta: metadata, timeout } = data
+  const session = context.getSession(sessionId)
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      params[key] = cypherToNative(value)
+    }
+  }
+
+  let result
+  try {
+    result = session.run(cypher, params, { metadata, timeout })
+  } catch (e) {
+    console.log('got some err: ' + JSON.stringify(e))
+    wire.writeError(e)
+    return
+  }
+
+  let id = context.addResult(result)
+
+  wire.writeResponse(responses.Result({ id }))
+}
