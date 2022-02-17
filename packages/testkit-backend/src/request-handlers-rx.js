5b70f95198acb0d7921db345479a8a3daf7f2351
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-02-17 18:36:36 +0100

    Add rx support for `ResultNext` and make tests on stub.session_run_parameters.test_session_run_parameters.TestSessionRunParameters pass

--- packages/testkit-backend/src/request-handlers-rx.js
+++ packages/testkit-backend/src/request-handlers-rx.js
@@ -1,5 +1,8 @@
 import * as responses from './responses.js';
 import neo4j from './neo4j.js';
+import {
+  cypherToNative
+} from './cypher-native-binders.js'
 
 // Handlers which didn't change depending
 export {
@@ -14,6 +17,7 @@ export {
   ResolverResolutionCompleted,
   GetRoutingTable,
   ForcedRoutingTableUpdate,
+  ResultNext
 } from './request-handlers.js';
 
 export function NewSession(context, data, wire) {
@@ -69,7 +73,116 @@ export function SessionRun (context, data, wire) {
     return
   }
 
+  result[Symbol.asyncIterator] = () => toAsyncIterator(result)
+
   let id = context.addResult(result)
 
   wire.writeResponse(responses.Result({ id }))
+}
+
+function toAsyncIterator(result) {
+  function queueObserver () {
+    function createResolvablePromise (){
+      const resolvablePromise = {}
+      resolvablePromise.promise = new Promise((resolve, reject) => {
+        resolvablePromise.resolve = resolve
+        resolvablePromise.reject = reject
+      });
+      return resolvablePromise;
+    }
+
+
+    function isError(elementOrError){
+      return elementOrError instanceof Error
+    }
+
+    const buffer = []
+    const promiseHolder = { resolvable: null }
+
+    const observer = {
+      next: (record) => {
+        observer._push({ done: false, value: record })
+      },
+      complete: (summary) => {
+        observer._push({ done: true, value: summary })
+      },
+      error: (error) => {
+        observer._push(error)
+      },
+      _push(element) {
+        if (promiseHolder.resolvable !== null) {
+          const resolvable = promiseHolder.resolvable
+          promiseHolder.resolvable = null
+          if (isError(element)) {
+            resolvable.reject(element)
+          } else {
+            resolvable.resolve(element)
+          }
+        } else {
+          buffer.push(element)
+        }
+      },
+      dequeue: async () => {
+        if (buffer.length > 0) {
+          const element = buffer.shift()
+          if (isError(element)) {
+              throw element
+          }
+          return element
+        }
+        promiseHolder.resolvable = createResolvablePromise()
+        return await promiseHolder.resolvable.promise
+      },
+      head: async () => {
+        if (buffer.length > 0) {
+          const element = buffer[0]
+          if (isError(element)) {
+              throw element
+          }
+          return element
+        }
+        promiseHolder.resolvable = createResolvablePromise()
+        try {
+          const element =  await promiseHolder.resolvable.promise
+          buffer.unshift(element)
+          return element
+        } catch (error) {
+          buffer.unshift(error)
+          throw error
+        }
+      },
+      get size () {
+        return buffer.length
+      }
+    }
+
+    return observer
+  }
+  const records = result.records()
+
+  const observer = queueObserver()
+  records.subscribe(observer);
+  
+  const state = {
+    finished: false
+  }
+
+  return {
+    next: async () => {
+      if (state.finished) {
+        return { done: true }
+      }
+      const next = observer.dequeue()
+      if (next.done) {
+        state.finished = next.done
+        state.summary = next.value
+      }
+      return next
+    },
+    return: async (value) => {
+      state.finished = true
+      state.summary = value
+      return { done: true, value: value }
+    }
+  }
 }
