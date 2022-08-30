91c8084a9af97fa36f9fe226edf18167c0b4bfce
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-08-30 19:04:15 +0200

    Fix serialization of broken objects in the driver logs
    Trying to logging broken objects was causing the driver fails in unexpected moments.
    This broken behaviour makes the driver fail different when the `debug` log is enabled.

---
+++ packages/core/test/json.test.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { json, newError } from '../src'
+import { createBrokenObject } from '../src/internal/util'
+
+describe('json', () => {
+  describe('.stringify', () => {
+    it('should handle objects created with createBrokenObject', () => {
+      const reason = newError('some error')
+      const broken = createBrokenObject(reason, { })
+
+      expect(json.stringify(broken)).toMatchSnapshot()
+    })
+
+    it('should handle objects created with createBrokenObject in list', () => {
+      const reason = newError('some error')
+      const broken = createBrokenObject(reason, { })
+
+      expect(json.stringify([broken])).toMatchSnapshot()
+    })
+
+    it('should handle objects created with createBrokenObject inside other object', () => {
+      const reason = newError('some error')
+      const broken = createBrokenObject(reason, { })
+
+      expect(json.stringify({
+        number: 1,
+        broken
+      })).toMatchSnapshot()
+    })
+  })
+})
--- packages/testkit-backend/src/skipped-tests/rx.js
+++ packages/testkit-backend/src/skipped-tests/rx.js
@@ -4,15 +4,6 @@ const skippedTests = [
   skip(
     'Throws after run insted of the first next because of the backend implementation',
     ifEquals('stub.disconnects.test_disconnects.TestDisconnects.test_disconnect_on_tx_begin')
-  ),
-  skip(
-    'Backend could not support this test',
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV4x4.test_unknown_zoned_date_time'),
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV4x4.test_unknown_zoned_date_time_patched'),
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV4x4.test_unknown_then_known_zoned_date_time'),
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV4x4.test_unknown_then_known_zoned_date_time_patched'),
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV5x0.test_unknown_then_known_zoned_date_time'),
-    ifEquals('stub.types.test_temporal_types.TestTemporalTypesV5x0.test_unknown_zoned_date_time')
   )
 ]
 
