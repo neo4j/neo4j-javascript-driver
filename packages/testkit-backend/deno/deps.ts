a7beadc5f71ceb28634fc1750d74902dff090d64
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-09-28 16:53:18 +0200

    Fix deps

--- packages/testkit-backend/deno/deps.ts
+++ packages/testkit-backend/deno/deps.ts
@@ -1,4 +1,4 @@
-export { iterateReader } from "https://deno.land/std@0.157.0/streams/conversion.ts";
+export { iterateReader } from "https://deno.land/std@0.119.0/streams/conversion.ts";
 export { default as Context } from "../src/context.js";
 export { getShouldRunTest } from "../src/skipped-tests/index.js";
 export { default as neo4j } from "../../neo4j-driver-deno/lib/mod.ts";
