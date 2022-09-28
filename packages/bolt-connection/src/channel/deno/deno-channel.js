1cbdecae5d063098c3f9df5986aae93be3a8fe77
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-09-28 17:42:28 +0200

    Disable eslint for Deno files

--- packages/bolt-connection/src/channel/deno/deno-channel.js
+++ packages/bolt-connection/src/channel/deno/deno-channel.js
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* eslint-env deno */
+/* eslint-disable */
 import ChannelBuffer from '../channel-buf'
 import { newError, internal } from 'neo4j-driver-core'
 import { iterateReader } from 'https://deno.land/std@0.157.0/streams/conversion.ts';
