8f5022e055d8ad438ca7572b81400f1b2a49a840
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-09-28 14:07:44 +0200

    Prevent write in a broken socket

--- packages/testkit-backend/deno/channel.ts
+++ packages/testkit-backend/deno/channel.ts
@@ -12,50 +12,71 @@ export async function* listen(port: number): AsyncIterable<TestkitClient> {
 
   for await (const conn of listener) {
     const id = clientId++;
-    const requests = () => readRequests(conn);
-    const reply = createReply(conn);
+    const { requests, reply } = setupRequestsAndReply(conn)
     yield { id, requests, reply };
   }
 }
 
-async function* readRequests(conn: Deno.Conn): AsyncIterable<TestkitRequest> {
-  let inRequest = false;
-  let requestString = "";
-  for await (const message of iterateReader(conn)) {
-    const rawTxtMessage = new TextDecoder().decode(message);
-    const lines = rawTxtMessage.split("\n");
-    for (const line of lines) {
-      switch (line) {
-        case "#request begin":
-          if (inRequest) {
-            throw new Error("Already in request");
-          }
-          inRequest = true;
-          break;
-        case "#request end":
-          if (!inRequest) {
-            throw new Error("Not in request");
-          }
-          yield JSON.parse(requestString);
-          inRequest = false;
-          requestString = "";
-          break;
-        case "":
-          // ignore empty lines
-          break;
-        default:
-          if (!inRequest) {
-            throw new Error("Not in request");
-          }
-          requestString += line;
-          break;
-      }
-    }
-  }
+interface State {
+  finishedReading: boolean
 }
 
-function createReply(conn: Deno.Conn) {
+function setupRequestsAndReply (conn: Deno.Conn) {
+  const state = { finishedReading: false }
+  const requests = () => readRequests(conn, state);
+  const reply = createReply(conn, state);
+  
+  return { requests, reply }
+}
+
+async function* readRequests(conn: Deno.Conn, state: State ): AsyncIterable<TestkitRequest> {
+  let inRequest = false;
+  let requestString = "";
+  try {
+    for await (const message of iterateReader(conn)) {
+      const rawTxtMessage = new TextDecoder().decode(message);
+      const lines = rawTxtMessage.split("\n");
+      for (const line of lines) {
+        switch (line) {
+          case "#request begin":
+            if (inRequest) {
+              throw new Error("Already in request");
+            }
+            inRequest = true;
+            break;
+          case "#request end":
+            if (!inRequest) {
+              throw new Error("Not in request");
+            }
+            yield JSON.parse(requestString);
+            inRequest = false;
+            requestString = "";
+            break;
+          case "":
+            // ignore empty lines
+            break;
+          default:
+            if (!inRequest) {
+              throw new Error("Not in request");
+            }
+            requestString += line;
+            break;
+        }
+      }
+    }
+  } finally {
+    state.finishedReading = true
+  }
+  
+
+}
+
+function createReply(conn: Deno.Conn, state: State) {
   return async function (response: TestkitResponse): Promise<void> {
+    if (state.finishedReading) {
+      console.warn('Discarded response:', response)
+      return
+    }
     const responseStr = JSON.stringify(
       response,
       (_, value) => typeof value === "bigint" ? `${value}n` : value,
