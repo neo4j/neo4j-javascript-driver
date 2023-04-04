4579f14de5a0a29698744d6584fab16cfc5e9c00
Antonio Barcelos <antonio.barcelos@neo4j.com>
2023-04-04 16:26:31 +0200

    Enables Deno specific tests in Core
    Core features which are environment dependent will need to be implemented.
    For testing this features, environment dependent tests need to be setup.
    
    The `deno` tests can be run by `npm run test::deno` and they need to be located under `./packages/core/test/deno`.
    The testkit pipeline is configured to run these tests only when configured for test `deno`.

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -152,9 +152,10 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/test/deno/"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
