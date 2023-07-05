d0a04d4c534908162d0320dd23efa3bd5d895a63
Antonio Barcelos <antonio.barcelos@neo4j.com>
2023-07-05 18:19:27 +0200

    Add more tests to the pool

--- packages/testkit-backend/src/skipped-tests/common.js
+++ packages/testkit-backend/src/skipped-tests/common.js
@@ -5,7 +5,11 @@ const skippedTests = [
     "Fixme: transactions don't prevent further actions after failure.",
     ifEquals('stub.tx_run.test_tx_run.TestTxRun.test_should_prevent_discard_after_tx_termination_on_run'),
     ifEquals('stub.tx_run.test_tx_run.TestTxRun.test_should_prevent_pull_after_tx_termination_on_pull'),
-    ifEquals('stub.tx_run.test_tx_run.TestTxRun.test_should_prevent_pull_after_tx_termination_on_run')
+    ifEquals('stub.tx_run.test_tx_run.TestTxRun.test_should_prevent_pull_after_tx_termination_on_run'),
+    ifEquals('stub.tx_run.test_tx_run.TestTxRun.test_should_prevent_run_after_tx_termination_on_run'),
+    ifEquals('stub.configuration_hints.test_connection_recv_timeout_seconds.TestDirectConnectionRecvTimeout.test_timeout_unmanaged_tx_should_fail_subsequent_usage_after_timeout'),
+    ifEquals('neo4j.test_tx_run_.TestTxRun.test_consume_after_commit'),
+    ifEquals('neo4j.test_tx_run_.TestTxRun.test_tx_configuration')
   ),
   skip(
     'Driver does not return offset for old DateTime implementations',
