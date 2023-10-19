750aa995da398e5ca4cd8a29ec40cc629cfa84f2
Antonio Barcelos <antonio.barcelos@neo4j.com>
2023-10-19 11:34:14 +0200

    Set version

--- packages/neo4j-driver-deno/lib/version.ts
+++ packages/neo4j-driver-deno/lib/version.ts
@@ -17,4 +17,4 @@
  * limitations under the License.
  */
 
-export default "5.0.0-dev" // Specified using --version when running generate.ts
+export default "5.14.0-alpha" // Specified using --version when running generate.ts
