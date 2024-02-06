9ca1ca0ef9b8f361d3a4ac52c63bf672b263d5b6
Antonio Barcélos <antonio.barcelos@neo4j.com>
2024-02-06 14:58:05 +0100

    Addressing comments in the PR

--- packages/benchkit-backend/src/config.js
+++ packages/benchkit-backend/src/config.js
@@ -1,6 +1,5 @@
 export default {
   backendPort: process.env.TEST_BACKEND_PORT || 9000,
-  requestTimeout: Number(process.env.TEST_REQ_TIMEOUT) || 5000,
   username: process.env.TEST_NEO4J_USER || 'neo4j',
   password: process.env.TEST_NEO4J_PASS || 'password',
   hostname: process.env.TEST_NEO4J_HOST || 'localhost',
