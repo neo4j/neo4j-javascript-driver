78a669fa8dc9b9947feaf763ff49b68bdb95adb2
MaxAake <61233757+MaxAake@users.noreply.github.com>
2024-09-18 14:10:35 +0200

    Update prefs.js

--- testkit/firefox_profile/prefs.js
+++ testkit/firefox_profile/prefs.js
@@ -1,3 +1,3 @@
 /* global user_pref */
-user_pref('network.dnsCacheExpiration', 0)
-user_pref('network.dnsCacheExpirationGracePeriod', 0)
+user_pref('network.dnsCacheExpiration', 0);
+user_pref('network.dnsCacheExpirationGracePeriod', 0);
