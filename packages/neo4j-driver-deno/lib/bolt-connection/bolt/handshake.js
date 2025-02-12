38851e3d00c4059425ce5a9a90bc5b3256eda31e
Max Gustafsson <61233757+MaxAake@users.noreply.github.com>
2025-02-12 13:26:25 +0100

    Fix handshake manifest parsing of future bolt versions (#1253)

--- packages/neo4j-driver-deno/lib/bolt-connection/bolt/handshake.js
+++ packages/neo4j-driver-deno/lib/bolt-connection/bolt/handshake.js
@@ -19,7 +19,7 @@ import { alloc } from '../channel/index.js'
 import { newError } from '../../core/index.ts'
 
 const BOLT_MAGIC_PREAMBLE = 0x6060b017
-const AVAILABLE_BOLT_PROTOCOLS = [5.8, 5.7, 5.6, 5.4, 5.3, 5.2, 5.1, 5.0, 4.4, 4.3, 4.2, 3.0] // bolt protocols the client will accept, ordered by preference
+const AVAILABLE_BOLT_PROTOCOLS = ['5.8', '5.7', '5.6', '5.4', '5.3', '5.2', '5.1', '5.0', '4.4', '4.3', '4.2', '3.0'] // bolt protocols the client will accept, ordered by preference
 const DESIRED_CAPABILITES = 0
 
 function version (major, minor) {
@@ -98,7 +98,7 @@ function handshakeNegotiationV2 (channel, buffer, log) {
   })
   for (let i = 0; i < versions.length; i++) {
     const version = versions[i]
-    if (AVAILABLE_BOLT_PROTOCOLS.includes(Number(version.major + '.' + version.minor))) {
+    if (AVAILABLE_BOLT_PROTOCOLS.includes(version.major + '.' + version.minor)) {
       major = version.major
       minor = version.minor
       break
