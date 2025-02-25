06e35b986dd72056234624d12a95fd4e6c3c4b4f
MaxAake <61233757+MaxAake@users.noreply.github.com>
2025-02-25 14:09:28 +0100

    more precise skipping

--- packages/testkit-backend/src/skipped-tests/rx.js
+++ packages/testkit-backend/src/skipped-tests/rx.js
@@ -7,7 +7,13 @@ const skippedTests = [
   ),
   skip(
     'Reactive driver does not send DISCARD on consume if records stream has been subscribed to',
-    ifStartsWith('stub.summary.test_summary')
+    ifStartsWith('stub.summary.test_summary.TestSummaryPlanDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryCountersDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryBasicInfoDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryGqlStatusObjects4x4Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryGqlStatusObjects5x6Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryNotifications4x4Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryNotifications5x6Discard')
   )
 ]
 
