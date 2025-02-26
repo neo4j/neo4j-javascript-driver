9d58fa6e968c3a5be1e3325b11521ab670d10ee6
Max Gustafsson <61233757+MaxAake@users.noreply.github.com>
2025-02-26 16:14:20 +0100

    Skip new summary tests on RX session (#1262)

--- packages/testkit-backend/src/skipped-tests/rx.js
+++ packages/testkit-backend/src/skipped-tests/rx.js
@@ -1,9 +1,19 @@
-import { skip, ifEquals } from './skip.js'
+import { skip, ifEquals, ifStartsWith } from './skip.js'
 
 const skippedTests = [
   skip(
     'Throws after run insted of the first next because of the backend implementation',
     ifEquals('stub.disconnects.test_disconnects.TestDisconnects.test_disconnect_on_tx_begin')
+  ),
+  skip(
+    'Reactive driver does not send DISCARD on consume if records stream has been subscribed to',
+    ifStartsWith('stub.summary.test_summary.TestSummaryPlanDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryCountersDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryBasicInfoDiscard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryGqlStatusObjects4x4Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryGqlStatusObjects5x6Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryNotifications4x4Discard'),
+    ifStartsWith('stub.summary.test_summary.TestSummaryNotifications5x6Discard')
   )
 ]
 
