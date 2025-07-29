c2646f3eab772624ed350e6557feb0e17039fb7e
ci <61233757+MaxAake@users.noreply.github.com>
2025-07-29 13:30:26 +0200

    avoid duplicate test names

--- packages/bolt-connection/test/bolt/bolt-protocol-v4x0.test.js
+++ packages/bolt-connection/test/bolt/bolt-protocol-v4x0.test.js
@@ -117,7 +117,7 @@ describe('#unit BoltProtocolV4x0', () => {
     expect(protocol.flushes).toEqual([false, true])
   })
 
-  it('should begin a transaction', () => {
+  it('should begin a transaction with set database name', () => {
     const database = 'testdb'
     const bookmarks = new Bookmarks([
       'neo4j:bookmark:v1:tx1',
