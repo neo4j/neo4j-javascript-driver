5f524813df0ce4ede5ef38d2299ae7c33c0576b5
ci <61233757+MaxAake@users.noreply.github.com>
2025-08-27 14:41:53 +0200

    remove browser tests

--- packages/neo4j-driver/gulpfile.babel.js
+++ packages/neo4j-driver/gulpfile.babel.js
@@ -36,7 +36,6 @@ const sharedNeo4j = require('./test/internal/shared-neo4j').default
 const stream = require('stream')
 const ts = require('gulp-typescript')
 const JasmineReporter = require('jasmine-spec-reporter').SpecReporter
-const karma = require('karma')
 const log = require('fancy-log')
 const JasmineExec = require('jasmine')
 
@@ -142,18 +141,6 @@ gulp.task('test-nodejs-integration', async () => {
   return runJasmineTests('#integration*')
 })
 
-gulp.task('run-browser-test-chrome', async function (cb) {
-  await sharedNeo4j.start()
-  runKarma('chrome', cb)
-})
-
-gulp.task('run-browser-test-firefox', async function (cb) {
-  await sharedNeo4j.start()
-  runKarma('firefox', cb)
-})
-
-gulp.task('run-browser-test', gulp.series('run-browser-test-firefox'))
-
 gulp.task('watch', function () {
   return watch(
     'src/**/*.js',
@@ -238,7 +225,7 @@ gulp.task('run-ts-declaration-tests', function (done) {
 
 gulp.task('all', gulp.series('nodejs', 'browser'))
 
-gulp.task('test-browser', gulp.series('start-neo4j', 'browser', 'run-browser-test'))
+gulp.task('test-browser', gulp.series('start-neo4j', 'browser'))
 
 gulp.task(
   'test',
@@ -276,17 +263,6 @@ function newJasmineConsoleReporter () {
       displayErrorMessages: true
     }
   })
-}
-
-function runKarma (browser, cb) {
-  new karma.Server(
-    {
-      configFile: path.join(__dirname, `/test/browser/karma-${browser}.conf.js`)
-    },
-    function (exitCode) {
-      exitCode ? process.exit(exitCode) : cb()
-    }
-  ).start()
 }
 
 function runJasmineTests (filterString) {
