ddcbeef98e655018f62bf5e02bae6f0c9dd6254b
ci <61233757+MaxAake@users.noreply.github.com>
2025-09-22 11:55:44 +0200

    additional tests

--- packages/core/test/vector-type.test.ts
+++ packages/core/test/vector-type.test.ts
@@ -26,6 +26,7 @@ describe('Vector', () => {
       ['BigInt64Array', BigInt64Array.from([BigInt(0)]), 'INT64'],
       ['Float32Array', Float32Array.from([0]), 'FLOAT32'],
       ['Float64Array', Float64Array.from([0]), 'FLOAT64']
+
     ])('should create vector from (%s)', (_, typedArray, expectedType) => {
       const vec = vector(typedArray)
       expect(vec.getType()).toEqual(expectedType)
@@ -39,9 +40,10 @@ describe('Vector', () => {
       ['Int32Array', Int32Array.from([3]), 'vector([3], 1, INTEGER32 NOT NULL)'],
       ['BigInt64Array', BigInt64Array.from([BigInt(0)]), 'vector([0], 1, INTEGER NOT NULL)'],
       ['Float32Array', Float32Array.from([0.5, 1.875]), 'vector([0.5, 1.875], 2, FLOAT32 NOT NULL)'],
-      ['Float32Array with special values', Float32Array.from([-Infinity, Infinity, NaN, -NaN]), 'vector([-Infinity, Infinity, NaN, NaN], 4, FLOAT32 NOT NULL)'],
+      ['Float32Array with special values', Float32Array.from([-Infinity, Infinity, NaN, -NaN, 1.401298464324817e-45, 3.4028235e38]), 'vector([-Infinity, Infinity, NaN, NaN, 1.401298464324817e-45, 3.4028234663852886e+38], 6, FLOAT32 NOT NULL)'],
       ['Float64Array', Float64Array.from([2]), 'vector([2], 1, FLOAT NOT NULL)'],
-      ['Float64Array with special values', Float64Array.from([-Infinity, Infinity, NaN, -NaN]), 'vector([-Infinity, Infinity, NaN, NaN], 4, FLOAT NOT NULL)']
+      ['Float64Array with special values', Float64Array.from([-Infinity, Infinity, NaN, -NaN, 2.2250738585072014e-308, 1.7976931348623157e308]), 'vector([-Infinity, Infinity, NaN, NaN, 2.2250738585072014e-308, 1.7976931348623157e+308], 6, FLOAT NOT NULL)'],
+      ['Float64Array of huge size', Float64Array.from([...Array(10000).keys()]), `vector([${[...Array(10000).keys()].join(', ')}], 10000, FLOAT NOT NULL)`]
     ])('should correctly stringify (%s)', (_, typedArray, expectedString) => {
       const vec = vector(typedArray)
       expect(vec.toString()).toEqual(expectedString)
