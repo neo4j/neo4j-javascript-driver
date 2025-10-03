c28fe0d48c657ca30c4c5c150d85a39cc7550f22
ci <61233757+MaxAake@users.noreply.github.com>
2025-10-03 14:37:44 +0200

    replacing usage of npm and lerna with pnpm

--- packages/neo4j-driver/docs/embed-docs.plugin.js
+++ packages/neo4j-driver/docs/embed-docs.plugin.js
@@ -1,19 +1,18 @@
 const fs = require('fs')
-let option;
+let option
 
 class EmbebDocsPlugin {
-  onStart(ev) {
+  onStart (ev) {
     option = ev.data.option
   }
 
-  onHandleDocs(ev) {
+  onHandleDocs (ev) {
     if (option.enabled) {
       const data = fs.readFileSync(option.path + '/index.json', 'utf-8')
       const embedDocs = JSON.parse(data.toString())
       ev.data.docs = [...ev.data.docs, ...embedDocs]
     }
   };
-
 }
 
-module.exports = new EmbebDocsPlugin();
+module.exports = new EmbebDocsPlugin()
