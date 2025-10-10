a998ef87d4874d78e55de3e9be05774978cb0307
ci <61233757+MaxAake@users.noreply.github.com>
2025-10-10 09:44:34 +0200

    run whole test in transaction to avoid interference

--- packages/neo4j-driver/test/vector-type.test.js
+++ packages/neo4j-driver/test/vector-type.test.js
@@ -49,24 +49,21 @@ describe('#integration vector type', () => {
   it('write and read vectors', async () => {
     if (protocolVersion.isGreaterOrEqualTo({ major: 6, minor: 0 }) && edition === 'enterprise') {
       const driver = driverGlobal
-
+      const session = driver.session()
       const bufferWriter = Uint8Array.from([1, 1])
-      const write = await driver.executeQuery('CREATE (p:Product) SET p.vector_from_array = $vector_from_array, p.vector_from_buffer = $vector_from_buffer', {
-        vector_from_array: neo4j.vector(Float32Array.from([1, 2, 3, 4])), // Typed arrays can be created from a regular list of Numbers
-        vector_from_buffer: neo4j.vector(new Int8Array(bufferWriter.buffer)) // Or from a bytebuffer
+      const res = await session.executeWrite(async (tx) => {
+        await tx.run('CREATE (p:Product) SET p.vector_from_array = $vector_from_array, p.vector_from_buffer = $vector_from_buffer', {
+          vector_from_array: neo4j.vector(Float32Array.from([1, 2, 3, 4])), // Typed arrays can be created from a regular list of Numbers
+          vector_from_buffer: neo4j.vector(new Int8Array(bufferWriter.buffer)) // Or from a bytebuffer
+        })
+        return await tx.run('MATCH (p:Product) RETURN p.vector_from_array as arrayVector, p.vector_from_buffer as bufferVector')
       })
-
-      console.error(write)
-
-      const read = await driver.executeQuery('MATCH (p:Product) RETURN p.vector_from_array as arrayVector, p.vector_from_buffer as bufferVector')
-
-      console.error(JSON.stringify(read))
-
-      const arrayVec = read.records[0].get('arrayVector').asTypedArray()
-      const bufferVec = read.records[0].get('bufferVector').asTypedArray()
+      const arrayVec = res.records[0].get('arrayVector').asTypedArray()
+      const bufferVec = res.records[0].get('bufferVector').asTypedArray()
 
       expect(arrayVec[0]).toBe(1)
       expect(bufferVec[1]).toBe(1)
+      session.close()
     }
   })
 })
