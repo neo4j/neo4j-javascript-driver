4770686531bfb19f55fdf3b6a59e9f6fcac5fbf1
ci <61233757+MaxAake@users.noreply.github.com>
2025-11-13 09:04:24 +0100

    switch browser tests to JSDOM environment

--- packages/neo4j-driver/test/browser.environment.js
+++ packages/neo4j-driver/test/browser.environment.js
@@ -14,11 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const { TestEnvironment: NodeEnvironment } = require('jest-environment-node')
+const { TestEnvironment: JSDOMEnvironment } = require('jest-environment-jsdom')
 const WebSocket = require('isomorphic-ws')
 const Config = require('./config')
 
-class BrowserEnvironment extends NodeEnvironment {
+class BrowserEnvironment extends JSDOMEnvironment {
   async setup () {
     await super.setup()
     this.global.WebSocket = WebSocket
