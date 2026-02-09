1c55a23418d9722a0eb961f2e63e342d524631f5
Max Gustafsson <61233757+MaxAake@users.noreply.github.com>
2026-02-09 16:23:56 +0100

    Improve Session.run docs with warnings (#1389)

--- packages/neo4j-driver-deno/lib/core/session.ts
+++ packages/neo4j-driver-deno/lib/core/session.ts
@@ -177,6 +177,17 @@ class Session {
    * Could be called with a query object i.e.: `{text: "MATCH ...", parameters: {param: 1}}`
    * or with the query and parameters as separate arguments.
    *
+   * This is an auto-commit query, meaning the server will commit the results without further input.
+   *
+   * The transaction is guaranteed to be committed anywhere between the start of
+   * it (i.e., calling :meth:`neo4j.Session.run`) and fully consuming the
+   * returned {@link Result}. The Result can be consumed as a promise or lazily using {@link Result#subscribe}.
+   *
+   * If the code moves on (i.e by calling {@link Session#close} or by starting another session)
+   * before the Result is fully consumed, it will be consumed in th background. However,
+   * in this case any error that occurs will be swallowed silently. Therefore, it is reccomended to always
+   * fully consume Results from Session.run.
+   *
    * @public
    * @param {mixed} query - Cypher query to execute
    * @param {Object} parameters - Map with parameters to use in query
