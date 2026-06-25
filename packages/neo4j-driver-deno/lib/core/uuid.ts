001a224ab0fb0e5f86041419f9874b7d78519c7a
MaxAake <61233757+MaxAake@users.noreply.github.com>
2026-06-25 13:58:54 +0200

    deno sync

--- packages/neo4j-driver-deno/lib/core/uuid.ts
+++ packages/neo4j-driver-deno/lib/core/uuid.ts
@@ -23,7 +23,7 @@ const uuidV4DashLocations = [3, 5, 7, 9]
 /**
  * Represents a Neo4j UUID value, stored as a Uint8Array of length 16, which can be gotten as a uuid string.
  * Created {@link UUID} objects are frozen with `Object.freeze()` in constructor and thus immutable.
- * 
+ *
  * @access public
  * @exports UUID
  * @class A class for writing/reading the Neo4j UUID type as a Uint8Array.
@@ -65,7 +65,7 @@ export default class UUID {
   /**
    * Parses a string representation of a uuid and creates a {@link UUID} from it.
    * Created {@link UUID} objects are frozen with `Object.freeze()` in constructor and thus immutable.
-   * 
+   *
    * @param {string} uuidString a base-16 encoded uuid string of the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx or without any dashes
    * @returns {UUID}
    */
@@ -95,7 +95,7 @@ Object.defineProperty(UUID.prototype, UUID_IDENTIFIER_PROPERTY, {
 /**
  * Creates a {@link UUID} from a uuid string or length 16 Uint8Array, allowing it to be sent as a parameter of a Neo4j query.
  * Created {@link UUID} objects are frozen with `Object.freeze()` in constructor and thus immutable.
- * 
+ *
  * @access public
  * @param {Uint8Array | string} rawUUID - a uuid string or length 16 Uint8Array with the values of the UUID.
  * @return {UUID} - The Neo4j UUID ready to be used as a query parameter
