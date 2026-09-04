091692c5da6fe5beb6bc22af5d06a6dc6aab2a11
MaxAake <61233757+MaxAake@users.noreply.github.com>
2026-09-04 13:06:21 +0200

    deno sync

--- packages/neo4j-driver-deno/lib/mod.ts
+++ packages/neo4j-driver-deno/lib/mod.ts
@@ -282,7 +282,7 @@ function driver (
           routingContext: parsedUrl.query
         })
     } else {
-      if (isEmptyObjectOrNull(parsedUrl.query) !== true) {
+      if (!isEmptyObjectOrNull(parsedUrl.query)) {
         throw new Error(
           `Parameters are not supported with none routed scheme. Given URL: '${url}'`
         )
